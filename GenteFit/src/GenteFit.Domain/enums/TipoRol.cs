d9b35f1a9e504a3d6f8f457c9d1915fd8bc4be08
CarrasDev <carrascoluque@gmail.com>
2025-10-22 23:53:03 +0200

    Añadir enum TipoRol

---
+++ GenteFit/src/GenteFit.Domain/enums/TipoRol.cs
@@ -0,0 +1,11 @@
+namespace GenteFit.Domain
+{
+    public enum TipoRol
+    {
+        Administrador = 1,
+        Encargado = 2,
+        Recepcionista = 3,
+        Cliente = 4,
+        Monitor = 5
+    }
+}

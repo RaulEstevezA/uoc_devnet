fd8a2a14c0220e73337d7bb06eb5a2f9314193fb
CarrasDev <carrascoluque@gmail.com>
2025-10-23 20:11:23 +0200

    puesto al dia

---
+++ GenteFit/src/model/entity/ListaEspera.cs
@@ -0,0 +1,15 @@
+namespace GenteFit.src.model.entity;
+
+public class ListaEspera
+{
+    public int Id { get; set; }
+
+    public int SesionId { get; set; }
+    public Sesion Sesion { get; set; } = null!;
+
+    public int ClienteId { get; set; }
+    public Cliente Cliente { get; set; } = null!;
+
+    public int Posicion { get; set; }
+    public DateTime CreadaEn { get; set; } = DateTime.UtcNow;
+}

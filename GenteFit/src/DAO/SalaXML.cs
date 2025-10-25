4d8e4decc6b6fdb6e2779c752f461647db6a567c
RaulEstevezA <121643646+RaulEstevezA@users.noreply.github.com>
2025-10-25 19:49:17 +0200

    Anyadidos archivos XML

---
+++ GenteFit/src/DAO/SalaXML.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using GenteFit.src.model.entity;
+using GenteFit.src.model.dao;
+
+public class SalaXml
+{
+    public static List<Sala> LeerSalasDesdeXml(string ruta)
+    {
+        var doc = XDocument.Load(ruta);
+
+        return doc.Root?
+            .Elements("Sala")
+            .Select(x => new Sala
+            {
+                Id = int.Parse(x.Element("Id")?.Value ?? "0"),
+                Nombre = x.Element("Nombre")?.Value ?? "",
+                AforoMax = int.Parse(x.Element("AforoMax")?.Value ?? "0"),
+                Disponible = bool.Parse(x.Element("Disponible")?.Value ?? "true")
+            })
+            .ToList() ?? new List<Sala>();
+    }
+
+    public static void ImportarSalasDesdeXml(string ruta, GenteFitDbContext context)
+    {
+        var salas = LeerSalasDesdeXml(ruta);
+
+        // Evitar duplicados si lo deseas (ejemplo por Nombre)
+        foreach (var sala in salas)
+        {
+            if (!context.Salas.Any(s => s.Id == sala.Id))
+            {
+                context.Salas.Add(sala);
+            }
+        }
+
+        context.SaveChanges();
+    }
+}
+

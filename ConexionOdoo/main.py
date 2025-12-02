918cf76b00a47d8f1dc2dc506d3c3b3091a43bdb
CarrasDev <carrascoluque@gmail.com>
2025-12-02 20:43:35 +0100

    explicacion breve

--- ConexionOdoo/main.py
+++ ConexionOdoo/main.py
@@ -6,3 +6,7 @@ from importar_clientes import importar_clientes_odoo
 if __name__ == "__main__":
     
     importar_clientes_odoo()
+
+
+    # Mi idea es crear un exe al cual podamos pasar un parametro que inicie la exportacion o la importacion.
+    # Este exe se llamará desde un proceso lanzado por la aplicación GenteFit.
